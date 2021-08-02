5773314ad24e3297de5ce6ef4bd220858b48ce38
Charles_Quan <quanzhch@163.com>
2021-08-02 22:43:20 +0800

    爬虫，使用xpath获取网站标题

--- web_crawler/mySpider/mySpider/spiders/itcast.py
+++ web_crawler/mySpider/mySpider/spiders/itcast.py
@@ -8,6 +8,14 @@ class ItcastSpider(scrapy.Spider):
     start_urls = ("http://www.itcast.cn/channel/teacher.shtml",)
 
     def parse(self, response):
-        filename = "teacher.html"
-        with open(filename, "w") as f:
-            f.write(response.body.decode())
+        # 保存页面的html内容
+        # filename = "teacher.html"
+        # with open(filename, "w") as f:
+        #     f.write(response.body.decode())
+
+        # 获取网站标题
+        context = response.xpath('/html/head/title/text()')
+
+        # 提取网站标题
+        title = context.extract_first()
+        print(title)
