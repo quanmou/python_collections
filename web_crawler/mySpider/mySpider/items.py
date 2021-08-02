71412f6fde866fd2730a4c966052fd55d9cb652b
Charles_Quan <quanzhch@163.com>
2021-08-02 22:11:17 +0800

    添加爬虫的测试网站

--- web_crawler/mySpider/mySpider/items.py
+++ web_crawler/mySpider/mySpider/items.py
@@ -9,4 +9,6 @@ import scrapy
 class MyspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field()
+    info = scrapy.Field()
