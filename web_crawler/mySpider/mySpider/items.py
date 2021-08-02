963ac61a947324654b1a4ec978f16b88b718b062
Charles_Quan <quanzhch@163.com>
2021-08-02 22:55:13 +0800

    爬虫，爬取网页的教师信息

--- web_crawler/mySpider/mySpider/items.py
+++ web_crawler/mySpider/mySpider/items.py
@@ -12,3 +12,10 @@ class MyspiderItem(scrapy.Item):
     name = scrapy.Field()
     title = scrapy.Field()
     info = scrapy.Field()
+
+
+class ItcastItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    title = scrapy.Field()
+    info = scrapy.Field()
--- web_crawler/mySpider/mySpider/spiders/itcast.py
+++ web_crawler/mySpider/mySpider/spiders/itcast.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import ItcastItem
 
 
 class ItcastSpider(scrapy.Spider):
@@ -15,7 +16,25 @@ class ItcastSpider(scrapy.Spider):
 
         # 获取网站标题
         context = response.xpath('/html/head/title/text()')
-
-        # 提取网站标题
-        title = context.extract_first()
+        title = context.extract_first()  # 提取网站标题
         print(title)
+
+        # 提取教师信息
+        items = []
+        for each in response.xpath("//div[@class='li_txt']"):
+            # 将我们得到的数据封装到一个 `ItcastItem` 对象
+            item = ItcastItem()
+            # extract()方法返回的都是unicode字符串
+            name = each.xpath("h3/text()").extract()
+            title = each.xpath("h4/text()").extract()
+            info = each.xpath("p/text()").extract()
+
+            # xpath返回的是包含一个元素的列表
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['info'] = info[0]
+
+            items.append(item)
+
+        # 直接返回最后数据
+        return items
