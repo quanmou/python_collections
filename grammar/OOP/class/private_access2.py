fe2348e25667ae8481ef1b8057479e2f4d480aa7
Charles_Quan <quanzhch@163.com>
2021-08-06 15:13:19 +0800

    将python2代码改为python3

--- grammar/OOP/class/private_access2.py
+++ grammar/OOP/class/private_access2.py
@@ -1,12 +1,16 @@
 class Test:
     def __init__(self):
+        self.name = "public"
         self.__name = "scolia"
 
     def a(self):
-        print self.__name
+        print(self.__name)
+
 
 a = Test()
 a.a()
 
-print a.__name
-print Test.__name
+print(a.name)
+print(Test.name)
+print(a.__name)
+print(Test.__name)
