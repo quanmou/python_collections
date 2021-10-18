aa3f3f7a145e5e892cd790b39ef6605d71ce6fae
Charles_Quan <quanzhch@163.com>
2021-10-18 15:11:03 +0800

    添加多进程模块

---
+++ modules/multi_processing/_process.py
@@ -0,0 +1,19 @@
+import os
+import time
+from multiprocessing import Process
+
+
+# 子进程要执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+    time.sleep(3)
+
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    print('Wait child process end')
+    p.join()
+    print('Child process end.')
