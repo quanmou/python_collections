c81127b8d77089ef4beb09caaedcdd1a9553f7e7
Charles_Quan <quanzhch@163.com>
2021-10-22 14:20:21 +0800

    pillow，补充图片缩放的注释

--- modules/pillow/resize_pic.py
+++ modules/pillow/resize_pic.py
@@ -9,8 +9,9 @@ im = Image.open(root_dir + '/images/test.jpeg')
 w, h = im.size
 print('Original image size: %s x %s' % (w, h))
 
-# 缩放50%
-im.thumbnail((w//2, h//2))
+# 缩放50%，thumbnail是直接在原图上修改，resize是生成新的图
+im.thumbnail((w//2, h//2), Image.ANTIALIAS)
+# im = im.resize((w//2, h//2), Image.ANTIALIAS)  # resize还可以用来放大，thumbnail只能缩小
 print('Resize image to: %s x %s' % (w//2, h//2))
 
 # 把缩放后的图像用jpeg格式保存
