b2a32b8fc1fdd88543a65fd2e59eefab2a72a41e
Charles_Quan <quanzhch@163.com>
2021-10-22 18:20:09 +0800

    matplotlib，绘制标签

--- modules/matplot/advance_sample.py
+++ modules/matplot/advance_sample.py
@@ -42,7 +42,20 @@ def plot_grid():
     plt.show()
 
 
+def plot_annotate():
+    """绘制标签"""
+    x = [2, 4, 6, 7, 8, 5, 4, 3]
+    y = [3, 6, 5, 8, 4, 3, 2, 4]
+    txt = ['我', '今', '晚', '上', '吃', '了', '个', '鲸']
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    plt.scatter(x, y)
+    for i in range(len(x)):
+        plt.annotate(txt[i], xy=(x[i], y[i]), xytext=(x[i] + 0.1, y[i] + 0.1))  # 这里xy是需要标记的坐标，xytext是对应的标签坐标
+    plt.show()
+
+
 if __name__ == "__main__":
     # plot_two_y_axis()
     # plot_ticks()
-    plot_grid()
+    # plot_grid()
+    plot_annotate()
