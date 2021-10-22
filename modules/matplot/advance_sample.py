2f2625cf16b0b49ea6b15b29f1f555440fd1bd1f
Charles_Quan <quanzhch@163.com>
2021-10-22 16:55:35 +0800

    matplotlib，添加高阶用法

---
+++ modules/matplot/advance_sample.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_two_y_axis():
+    """绘制两个y轴"""
+    x = np.arange(0, 6)
+    y1 = [30481, 12583, 51, 9, 2, 2]
+    y2 = [0.0065, 0.016, 0.039, 0, 0, 0]
+
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()  # 做镜像处理
+    ax1.plot(x, y1, 'g-')
+    ax2.plot(x, y2, 'b--')
+
+    ax1.set_xlabel('X data')  # 设置x轴标题
+    ax1.set_ylabel('total_count', color='g')  # 设置Y1轴标题
+    ax2.set_ylabel('bad_rate', color='b')  # 设置Y2轴标题
+
+    plt.show()
+
+
+def plot_ticks():
+    """更改x轴和y轴的刻度频率"""
+    x = [0, 5, 9, 10, 15]
+    y = [0, 1, 2, 3, 4]
+    plt.plot(x, y)
+    plt.xticks(np.arange(min(x), max(x) + 1, 1.0))
+    plt.show()
+
+
+def plot_grid():
+    """绘制网格线"""
+    x = np.array([1, 2, 3, 4])
+    y = np.array([1, 4, 9, 16])
+
+    plt.title("Grid Test")
+    plt.xlabel("x - label")
+    plt.ylabel("y - label")
+    plt.plot(x, y)
+    plt.grid(axis='both', color='r', linestyle='--', linewidth=0.5)  # axis可选值为x、y、both
+    plt.show()
+
+
+if __name__ == "__main__":
+    # plot_two_y_axis()
+    # plot_ticks()
+    plot_grid()
